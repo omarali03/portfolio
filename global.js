5dd5a591cd7b9885de2b20681258c5f5cc83eb17
omarali03 <omarthekid4@gmail.com>
2025-01-25 18:07:41 -0800

    Update global.js

--- global.js
+++ global.js
@@ -3,3 +3,9 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Get an array of all nav links
+const navLinks = $$("nav a");
+
+// Log the array to the console to verify
+console.log(navLinks);
