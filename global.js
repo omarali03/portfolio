341422be40b83d706817a53751772aad63a43ce7
omarali03 <omarthekid4@gmail.com>
2025-01-31 19:11:32 -0800

    Update global.js

--- global.js
+++ global.js
@@ -85,19 +85,19 @@ if (savedColorScheme) {
 
 export async function fetchJSON(url) {
   try {
-      const response = await fetch(url);  // Remove recursive call
+      const response = await fetch(url);
       
       if (!response.ok) {
           throw new Error(`Failed to fetch projects: ${response.statusText}`);
       }
 
-      const data = await response.json(); // Parse JSON
-      return data; // Return data after parsing
-
+      const data = await response.json();
+      return data;
   } catch (error) {
       console.error('Error fetching or parsing JSON data:', error);
   }
 }
+
 
 
   
