74f9f4e3c7c77033e475c4eb3845b59fda1db586
omarali03 <omarthekid4@gmail.com>
2025-01-31 22:48:58 -0800

    Update global.js

--- global.js
+++ global.js
@@ -158,6 +158,22 @@ export async function fetchGithubData(username) {
   }
 }
 
+export async function fetchGitHubData(omarali03) {
+  try {
+      const response = await fetch(`https://api.github.com/users/${omarali03}`);
+      
+      if (!response.ok) {
+          throw new Error(`Failed to fetch GitHub data: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      return data;
+
+  } catch (error) {
+      console.error("Error fetching GitHub data:", error);
+  }
+}
+
 
 
   
