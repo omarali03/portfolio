ab1a0c759cb8b9daf9c98305dfe7501e9f85053d
omarali03 <omarthekid4@gmail.com>
2025-02-20 14:34:15 -0800

    Update project.js

--- projects/project.js
+++ projects/project.js
@@ -1,10 +1,10 @@
 console.log("project.js is loaded and running!");
 import { fetchJSON, renderProjects } from '../global.js';
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+//import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 async function loadProjects() {
     try {
-        const projects = await fetchJSON('/portfolio/lib/projects.json'); // Adjust fetch path if needed
+        const projects = await fetchJSON('../projects.json');
         const projectsContainer = document.querySelector('.projects');
         renderProjects(projects, projectsContainer, 'h2');
 
