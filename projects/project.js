7c442bdbae55f28ae4ef726b02e0028b05626545
omarali03 <omarthekid4@gmail.com>
2025-02-20 15:11:45 -0800

    Update project.js

--- projects/project.js
+++ projects/project.js
@@ -4,7 +4,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 async function loadProjects() {
     try {
-        const projects = await fetchJSON('../projects.json');
+        const projects = await fetchJSON('lib/projects.json');
         const projectsContainer = document.querySelector('.projects');
         renderProjects(projects, projectsContainer, 'h2');
 
