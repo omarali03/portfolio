096c16368ff4f23c4167e681504049dc441f9caa
omarali03 <omarthekid4@gmail.com>
2025-02-25 02:20:23 -0800

    Update project.js

--- projects/project.js
+++ projects/project.js
@@ -4,40 +4,26 @@ import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("D3 Pie Chart script is running!");
-
     const svg = d3.select("#projects-plot");
 
-    // Define pie chart data (33% and 66% slices)
-    let data = [1, 2];
+    let data = [1, 2, 3, 4, 5, 5];
 
-    // Calculate total sum of the data
-    let total = data.reduce((sum, d) => sum + d, 0);
+    let colorScale = d3.scaleOrdinal(d3.schemeTableau10);
 
-    // Define arc generator
+    let sliceGenerator = d3.pie();
+    let arcData = sliceGenerator(data);
+
     let arcGenerator = d3.arc()
         .innerRadius(0)  
         .outerRadius(50);
 
-    // Compute start and end angles
-    let angle = 0;
-    let arcData = [];
-
-    for (let d of data) {
-        let endAngle = angle + (d / total) * 2 * Math.PI;
-        arcData.push({ startAngle: angle, endAngle });
-        angle = endAngle;
-    }
-
-    // Generate paths and append them to the SVG
     svg.selectAll("path")
         .data(arcData)
         .enter()
         .append("path")
         .attr("d", arcGenerator)
-        .attr("fill", (d, i) => i === 0 ? "blue" : "orange");
+        .attr("fill", (d, i) => colorScale(i)); // Assign a unique color to each slice
 });
-
 
 
 async function loadProjects() {
